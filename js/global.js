5c79fde4e7482cee5a6794e3fc297313cb069ce3
Hancharou.Uladzislau <hancharou@rncpmt.by>
2024-02-13 11:57:31 +0300

    кнопки тип запроса и области

--- js/global.js
+++ js/global.js
@@ -18,20 +18,37 @@ $(document).ready(function () {
                         console.log(filteredRes);
 
                         filteredRes.forEach(item => {
+                            let replyMarkup;
                             let answer;
-                            if (item.message.text.toLowerCase().includes("привет")) {
-                                answer = "Здравствуйте, " + (item.message["from"].first_name === "NaN" ? item.message["from"].last_name : item.message["from"].first_name);
+                            if (item.message.text.toLowerCase().includes("привет") || item.message.text.toLowerCase().includes("/start")) {
+                                answer = "Здравствуйте, " + (item.message["from"].first_name === "NaN" ? item.message["from"].last_name : item.message["from"].first_name + "\nВыберите тип запроса");
+                                let buttons = [
+                                    [{ text: "Жалоба" }, { text: "Предложение" }],
+                                    [{ text: "Вопрос" }]
+                                ];
+                                replyMarkup = { keyboard: buttons, resize_keyboard: true };
                             }
                             else if (item.message.text.toLowerCase().includes("пока")){
                                     answer = "До свидания, " + (item.message["from"].first_name === "NaN" ? item.message["from"].last_name : item.message["from"].first_name);
-                            }else {
+                            }else if (item.message.text.toLowerCase().includes("жалоба") ||
+                                item.message.text.toLowerCase().includes("предложение") ||
+                                item.message.text.toLowerCase().includes("вопрос")){
+                                answer = "Выберите область";
+                                let buttons = [
+                                    [{ text: "Брестская" }, { text: "Витебская" }],
+                                    [{ text: "Гомельская" }], [{ text: "Гродненская" }],
+                                    [{ text: "Могилевская" }], [{ text: "Минская" }]
+                                ];
+                                replyMarkup = { keyboard: buttons, resize_keyboard: true };
+                            }
+                            else {
                                 answer = "ответ еще не существует";
                             }
 
                             $.ajax({
                                 url: 'sendMsg.php',
                                 type: 'GET',
-                                data: {idChat: item.message.chat.id, message: answer},
+                                data: {idChat: item.message.chat.id, message: answer, reply_markup: JSON.stringify(replyMarkup)},
                                 success:  (response) => {
                                 console.log(response);
                                 lastRes = res;
--- sendMsg.php
+++ sendMsg.php
@@ -4,8 +4,24 @@ $chat_id = $_GET["idChat"];
 
 $textMessage = $_GET["message"];
 $textMessage = urlencode($textMessage);
+$replyMarkup = $_GET['reply_markup'];
 
 $urlQuery = "https://api.telegram.org/bot". $token ."/sendMessage?chat_id=". $chat_id ."&text=" . $textMessage;
 
-$result = file_get_contents($urlQuery);
+$data = array(
+    'chat_id' => $chat_id,
+    'text' => $textMessage,
+    'reply_markup' => $replyMarkup
+);
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($urlQuery, false, $context);
 echo "ok";
