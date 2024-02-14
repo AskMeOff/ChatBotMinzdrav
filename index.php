5560cc81cb554d7ededa0fc88a5e6d6b7e37bf1d
NikitaSydykov <nkt.sdkv.1996@gmail.com>
2024-02-14 09:24:31 +0300

    WEB HOOKS PUDGE

--- index.php
+++ index.php
@@ -1,8 +1,97 @@
-<html>
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-</head>
-<body>
-    <script src="js/global.js"></script>
-</body>
-</html>
+<?php
+
+$data = json_decode(file_get_contents('php://input'), TRUE);
+//пишем в файл лог сообщений
+file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+
+define('TOKEN', '6829954140:AAEKsopPLCe3C5xCTtk1RaHj8mV6R7OhEMc');
+
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
+
+
+switch ($message) {
+    case '/start':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'Привет! Я бот Telegram.'];
+        break;
+    case 'да':
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Что вы хотите заказать?',
+            'reply_markup'  => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Яблоки'],
+                        ['text' => 'Груши'],
+                    ],
+                    [
+                        ['text' => 'Лук'],
+                        ['text' => 'Чеснок'],
+                    ]
+                ]
+            ]
+        ];
+        break;
+    case 'нет':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'Приходите еще'];
+        break;
+    case 'яблоки':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'заказ принят!'];
+        break;
+    case 'груши':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'заказ принят!'];
+        break;
+    case 'лук':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'заказ принят!'];
+        break;
+    case 'чеснок':
+        $method = 'sendMessage';
+        $send_data = ['text' => 'заказ принят!'];
+        break;
+    default:
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Вы хотите сделать заказ?',
+            'reply_markup'  => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Да'],
+                        ['text' => 'Нет'],
+                    ]
+                ]
+            ]
+        ];
+}
+
+$send_data['chat_id'] = $data['chat'] ['id'];
+
+$res = sendTelegram($method, $send_data);
+
+
+
+
+function sendTelegram($method, $data, $headers = [])
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
+    ]);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+}
+
+?>
